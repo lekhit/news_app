d4788b4e227039f1593cad1517262c121f5c050c
lekhit <82832791+lekhit@users.noreply.github.com>
2022-06-06 11:55:38 +0530

    write update news

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,62 +1,69 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 
-import Box from '@mui/material/Box';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import Mycard from './card';
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Mycard from "./card";
 //import Masonry from '@mui/lab/Masonry';
-import MyJson from '~/sample.json';
-import Grid from '@mui/material/Grid';
-import Navbar from './navBar';
+import MyJson from "../../sample.json";
+import Grid from "@mui/material/Grid";
+import Navbar from "./navBar";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-
-
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
-  backgroundColor:"lightblue",
+  textAlign: "center",
+  backgroundColor: "lightblue",
   color: theme.palette.text.secondary,
-  '&:hover': {
-    backgroundColor: 'blue',
-    opacity: [0.9, 0.8, 0.7],
+  "&:hover": {
+    backgroundColor: "blue",
+    opacity: [0.9, 0.8, 0.7]
   },
-  'onClick':{}
+  onClick: {}
 }));
 
 export default function FixedColumns() {
-  const [articles,setArticles]=useState(MyJson.articles);
+  const [articles, setArticles] = useState(MyJson.articles);
+  const [page, setPage] = useState(1);
+
   const updateNews = async () => {
-const url=``
+    const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=4d3f11b21d3e48018dfa0e36e8b5c7e8`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    setArticles(parsedData.articles);
   };
 
-
   //const { articles } = MyJson;
+
   return (
     <>
-    <Navbar/>
-    <Box sx={{ minHeight: 253 }}>
-      <Grid container   justifyContent="space-around"
-  spacing={0.5}
-  alignItems="stretch"
-  >
-    <InfiniteScroll
+      <Navbar />
+      <Box sx={{ minHeight: 253 }}>
+        <Grid
+          container
+          justifyContent="space-around"
+          spacing={0.5}
+          alignItems="stretch"
+        >
+          {/* { <InfiniteScroll
           dataLength={this.state.items.length}
           next={this.fetchMoreData}
           hasMore={true}
           loader={<h4>Loading...</h4>}
-        >
-        {articles.map((height, index) => (
-          <Grid key={index} sx={{ p: 2 }}>
-           <Item> <Mycard key={index} article={height} /></Item>
-          </Grid>
-        ))}
-         </InfiniteScroll>
-      </Grid>
-    </Box>
-
+        > } */}
+          {articles.map((height, index) => (
+            <Grid key={index} sx={{ p: 2 }}>
+              <Item>
+                {" "}
+                <Mycard key={index} article={height} />
+              </Item>
+            </Grid>
+          ))}
+          {/* </InfiniteScroll> */}
+        </Grid>
+      </Box>
     </>
   );
 }
